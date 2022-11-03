b22951b6d82a055ba6ec2aa00c6f57dbf129f05b
lszweda <lszweda@psi.pl>
2022-11-03 17:31:50 +0100

    adding variables from env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,13 @@
 import { HardhatUserConfig, subtask, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
+require('dotenv').config({path: './process.env'});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
 };
+
+const { GOERLI_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 task("deploy-testnets", "Deploys contract on a provided network")
      .setAction(async () => {
@@ -44,15 +47,15 @@ module.exports = {
     localhost: {
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/4d5caad6cbc645eba02a8cd5dc0036bb",
-      accounts: ['de7d5d62886cdbd7e2709c4f24ed5bc5e1e98a9414fcd8492601ac3c59fdf523']
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
     }
   },
 
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at <https://etherscan.io/>
-    apiKey: "IPXGIADUZ3GZ7ZIU3ZH6I8VNEZMY6MRZK4"
+    apiKey: ETHERSCAN_API_KEY
   },
 
   solidity: {
